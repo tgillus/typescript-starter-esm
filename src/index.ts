0d65e17369d11f9ba8c3ca90d2a948c8fc4eca87
Tramaine Gillus <tramaine.gillus@gmail.com>
2022-03-30 22:03:13 -0400

    chore: uncomment code

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,24 @@ import { ListBucketsCommand, S3Client } from '@aws-sdk/client-s3';
 import { LogService } from './log/log-service.js';
 import { CorrelationId } from './log/correlation-id.js';
 
-// console.log(add(1, 2));
+console.log(add(1, 2));
 
-// const { stdout } = await execa('echo', ['unicorns']);
-// console.log(stdout);
+const { stdout } = await execa('echo', ['unicorns']);
+console.log(stdout);
 
-// console.log(_.capitalize('foo'));
+console.log(_.capitalize('foo'));
 
-// const s3Client = new S3Client({});
-// const response = await s3Client.send(new ListBucketsCommand({}));
+const s3Client = new S3Client({});
+const response = await s3Client.send(new ListBucketsCommand({}));
 
-// console.log(response.Buckets);
+console.log(response.Buckets);
 
-// function foo<T>(val: T | number): val is number {
-//   return typeof val === 'number';
-// }
+function foo<T>(val: T | number): val is number {
+  return typeof val === 'number';
+}
 
-// console.log(foo(true));
-// console.log(foo(5));
+console.log(foo(true));
+console.log(foo(5));
 
 console.log(CorrelationId.id);
 LogService.build().somethingHappended();
